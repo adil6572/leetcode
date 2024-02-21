190bef60802fb73460d905d2523e071ea6112316
Aadil Shaikh <123252545+adil6572@users.noreply.github.com>
2024-02-21 18:37:33 +0530

    Time: 15 ms (79.61%) | Memory: 11.7 MB (53.17%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x==0 or x==1:
+            return x
+
+        start=1
+        end=x
+
+        while start <=end:
+            mid=start +  ( end-start) // 2
+            if mid * mid > x :
+                end=mid-1
+            elif mid * mid == x:
+                return mid
+            else:
+                start=mid+1
+        
+        return end
+        
